95adf3ff304315c0d569aae27ac4513a7f0f6643
martinrein <martin.rein.toledano@gmail.com>
2020-09-18 00:15:00 +0800

    Added root title

--- registration.py
+++ registration.py
@@ -1,15 +1,17 @@
 from tkinter import *
 
 root = Tk()
+root.title("Registration")
 
 e = Entry(root, width=50)
 e.pack()
 
 def myClick():
     name = e.get()
-    myLabel = Label(root, text = name)
+    myLabel = Label(root, text = name).grid(row=1, column=0)
     myLabel.pack()
 
+#label_name = Label(root, text="hello").grid(row=1, column=0)
 myButton = Button(root, text="Submit", command = myClick)
 myButton.pack()
 
