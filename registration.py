22a90d87daee5ae27d45eea5c26e3acb177f04c5
martinrein <martin.rein.toledano@gmail.com>
2020-09-20 18:59:13 +0800

    Able to save inputs to a text file.

--- registration.py
+++ registration.py
@@ -21,10 +21,10 @@ label_3 = Label(frame_register_form, text="Email", width=10,font=("bold",12))
 label_3.place(relx=0.05, rely=0.15)
 label_4 = Label(frame_register_form, text="Address", width=10,font=("bold",12))
 label_4.place(relx=0.07, rely=0.25)
-label_5 = Label(frame_register_form, text="Barber", width=10,font=("bold",12))
-label_5.place(relx=0.06, rely=0.35)
-label_6 = Label(frame_register_form, text="Date", width=10,font=("bold",12))
-label_6.place(relx=0.05, rely=0.45)
+# label_5 = Label(frame_register_form, text="Barber", width=10,font=("bold",12))
+# label_5.place(relx=0.06, rely=0.35)
+# label_6 = Label(frame_register_form, text="Date", width=10,font=("bold",12))
+# label_6.place(relx=0.05, rely=0.45)
 
 # Create Entry Widgets
 entry_1 = Entry(frame_register_form, width=30)
@@ -36,26 +36,24 @@ entry_3.place(relx=0.6, rely=0.175, anchor=CENTER)
 entry_4 = Entry(frame_register_form, width=30)
 entry_4.place(relx=0.6, rely=0.275, anchor=CENTER)
 
-def myClick():
-    name = entry_1.get()
-    myLabel = Label(root, text = name, padx=20, pady=20)#.grid(row=0, column=0)
-    myLabel.pack()
+def get_customer():
+    customer = 'Name: ' + entry_1.get() + '\nContact No.: ' + entry_2.get() + '\nEmail: ' + entry_3.get() + '\nAddress: ' + entry_4.get() + '\n\n'
+    save_to_file(customer)
 
-def popupConfirm():
+# Saves the registered user's information to a text file.
+def save_to_file(customer):
+    text_file = open("registered_users.txt", 'a')
+    text_file.write(customer)
+    text_file.close()
+
+def submit_registration():
     response = messagebox.askokcancel("Confirm Registration","Do you want to submit your registration?")
-    #Label(root,text=response).pack()
-
-    if response == 1:
+    
+    if response == 1:   # If response is 'OK'
+        get_customer()
         root.quit()
-        # open visual aid
 
-# def saveUser():
-#     text_file = open("registered_users.txt", 'a')
-#     text_file.append(.get())
-#     text_file.close()
-
-myButton = Button(root, text="Submit", width=20, command = popupConfirm)
+myButton = Button(root, text="Submit", width=20, command = submit_registration)
 myButton.place(relx=0.5, rely=0.95, anchor=CENTER)
-#myButton.pack()
 
 root.mainloop()
